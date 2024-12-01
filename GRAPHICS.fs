ccea6cabe5918ae44e638f6182538dc5c45aa0d7
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2024-12-01 17:45:55 +0100

    Add files via upload

--- GRAPHICS.fs
+++ GRAPHICS.fs
@@ -1,3 +1,28 @@
+\ *********************************************************************
+\ Graphics tests
+\    Filename:      GRAPHICS.fs
+\    Date:          09 mar 2023
+\    Updated:       01 dec 2024
+\    File Version:  1.0
+\    MCU:           eForth Windows
+\    Copyright:     Marc PETREMANN
+\    Author:        Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
 
 
-include graphics/main.fs
+
+
+\ load windows extensions
+needs tools/dumpTool.fs
+
+
+\ load graphics extensions
+needs extensions/Gdi32-definitions.fs
+
+
+\ load windows extensions
+needs extensions/User32-definitions.fs
+
+
+needs graphics/main.fs
