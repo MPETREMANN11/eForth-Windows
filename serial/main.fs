ac1b41da87b0d671c76c87b2c059bbd99d9f8907
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2024-12-25 15:33:49 +0100

    Add files via upload

--- serial/main.fs
+++ serial/main.fs
@@ -2,7 +2,7 @@
 \ main file for serial COMx tests
 \    Filename:      main.fs
 \    Date:          02 dec 2024
-\    Updated:       08 dec 2024
+\    Updated:       17 dec 2024
 \    File Version:  1.0
 \    MCU:           eForth Windows
 \    Copyright:     Marc PETREMANN
@@ -15,4 +15,13 @@
 
 needs serial.fs
 
-needs RWserial.fs
+\ needs RWserial.fs
+
+needs RWtoLoRa.fs
+
+needs tests.fs
+
+
+
+
+
